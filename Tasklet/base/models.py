65d3583d86b719ddbe72c8ee89777273600af6ca
Abhimanyu Sharma <abhisea12@gmail.com>
2022-03-20 22:09:56 -0700

    Django Project Underway

---
+++ Tasklet/base/models.py
@@ -0,0 +1,27 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here
+
+class Category(models.Model):
+     Header = models.CharField(max_length=250, null=True, blank=True)
+
+     def __str__(self):
+         return f"{self.Header}"
+     
+
+class Task(models.Model):
+     Task_Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+     title = models.CharField(max_length=250)
+     description = models.TextField(null=True, blank=True)
+     complete = models.BooleanField(default=False)
+     created = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+          return f"{self.title}"
+
+     class Meta:
+          ordering = ['complete'] 
+
